6f4c5d160838647cf33a763f5428d3c1065e9682
Douglas Lima <Douglasproglima@gmail.com>
2022-06-25 11:42:14 -0300

    Stating projects Clean Architecture

---
+++ gelateca.solidaria/gelateca.solidaria.webui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gelateca.solidaria.webui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
